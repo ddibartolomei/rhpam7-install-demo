702beead07f93953fdbfd369a9f2c9ccdb4bfb01
Daniele Di Bartolomei <daniele.dibartolomei@redhat.com>
2019-12-03 19:10:10 +0100

    Parametric project prefix and suffix for init-openshift.sh

--- init-openshift.sh
+++ init-openshift.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+if [[ $# -eq 0 ]]; then
+    SCRIPT_NAME=`basename "$0"`
+    echo "Usage:"
+    echo "./$SCRIPT_NAME <project-name-prefix> <project-name-suffix>"
+    echo "  Openshift target project will be <project-name-prefix>-<project-name-suffix>"
+    echo
+    echo "Example usage:"
+    echo "  ./$SCRIPT_NAME rhpam7-install demo"
+    exit 0
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
 . ./init-properties.sh
 
 command -v oc >/dev/null 2>&1 || {
@@ -14,8 +30,9 @@ function echo_header() {
   echo "########################################################################"
 }
 
-PRJ_DEMO="rhpam7-install"
-PRJ_DEMO_NAME=$(./support/openshift/provision.sh info $PRJ_DEMO | awk '/Project name/{print $3}')
+PRJ_PREFIX=$1
+PRJ_SUFFIX=$2
+PRJ_DEMO_NAME=$(./support/openshift/provision.sh info $PRJ_PREFIX | awk '/Project name/{print $3}')
 
 # Check if the project exists
 oc get project $PRJ_DEMO_NAME > /dev/null 2>&1
@@ -23,7 +40,7 @@ PRJ_EXISTS=$?
 
 if [ $PRJ_EXISTS -eq 0 ]; then
    echo_header "$PRJ_DEMO_NAME project already exists. Deleting project."
-   ./support/openshift/provision.sh delete $PRJ_DEMO
+   ./support/openshift/provision.sh delete $PRJ_PREFIX
    # Wait until the project has been removed
    echo_header "Waiting for OpenShift to clean deleted project."
    sleep 20
@@ -35,5 +52,5 @@ else if [ ! $PRJ_EXISTS -eq 1 ]; then
 fi
 
 echo_header "Provisioning $PRJ_DEMO_NAME."
-./support/openshift/provision.sh setup $PRJ_DEMO --with-imagestreams
+./support/openshift/provision.sh setup $PRJ_PREFIX --with-imagestreams --project-suffix $PRJ_SUFFIX
 echo_header "Setup completed."
